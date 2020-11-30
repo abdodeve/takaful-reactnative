52ba63b70ffdd1e5b7511df0b3876cc0b3d1bb6c
Abdelhadi Habchi <abdelhadi.deve@gmail.com>
2020-11-30 19:19:36 +0100

    Boilerplate dirty Workflow

--- App.tsx
+++ App.tsx
@@ -1,21 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Button, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createStackNavigator,
+  StackScreenProps,
+} from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
-export default function App() {
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+  TopTab: undefined;
+};
+
+type Props = StackScreenProps<RootStackParamList>;
+
+function DetailsScreen({ route, navigation }: Props) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Details!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function HomeScreen({ route, navigation }: Props) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+function SettingsScreen({ route, navigation }: Props) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+function TopTabScreen({ route, navigation }: Props) {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>TopTab screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+
+const TopTabStack = createStackNavigator();
+const TopTabNavigator = createMaterialTopTabNavigator();
+
+function TopTabNavigatorScreen() {
+  return (
+    <TopTabNavigator.Navigator>
+      <TopTabNavigator.Screen name="TopTab" component={TopTabScreen} />
+      <TopTabNavigator.Screen name="Details" component={DetailsScreen} />
+    </TopTabNavigator.Navigator>
+  );
+}
+
+function TopTabStackScreen() {
+  return (
+    <TopTabStack.Navigator>
+      <TopTabStack.Screen name="Home" component={TopTabNavigatorScreen} />
+    </TopTabStack.Navigator>
+    // <TopTabNavigator.Navigator>
+    //   <TopTabNavigator.Screen name="TopTab" component={TopTabScreen} />
+    //   <TopTabNavigator.Screen name="Details" component={DetailsScreen} />
+    // </TopTabNavigator.Navigator>
+  );
+}
+
+const HomeStack = createStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+const SettingsStack = createStackNavigator();
+
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+      <SettingsStack.Screen name="Details" component={DetailsScreen} />
+    </SettingsStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Settings" component={SettingsStackScreen} />
+        <Tab.Screen name="TopTab" component={TopTabStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
