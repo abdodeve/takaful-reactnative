72689a4471e9cb0b10d94b584d073c4e34914368
Abdelhadi Habchi <abdelhadi.deve@gmail.com>
2020-12-18 02:00:14 +0100

    Design BottomBar buttons

--- src/Navigation/MainStack/index.tsx
+++ src/Navigation/MainStack/index.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Layout, Text, useTheme } from "@ui-kitten/components";
 
+import { IconX, ICON_TYPE } from "../../Icons";
 import HomeStack from "./HomeStack";
 import MyAnnouncementsStack from "./MyAnnouncementsStack";
 import { Routes } from "./../Routes";
@@ -9,31 +11,96 @@ import { Routes } from "./../Routes";
 const Tab = createBottomTabNavigator();
 
 export default () => {
+  const theme = useTheme();
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      initialRouteName="Main"
+      tabBarOptions={{
+        activeTintColor: theme["color-primary-500"],
+        inactiveTintColor: theme["color-basic-600"],
+      }}
+    >
       <Tab.Screen
         name={Routes.HOME_STACK}
         options={{
-          title: "Dons",
+          tabBarLabel: "Dons",
+          tabBarIcon: ({ color, size }) => (
+            <IconX
+              name="gift"
+              color={color}
+              size={size}
+              origin={ICON_TYPE.FONT_AWESOME5}
+              style={{}}
+            />
+          ),
         }}
         component={HomeStack}
       />
       <Tab.Screen
         name="Demandes"
         options={{
-          title: "Demandes",
+          tabBarLabel: "Demandes",
+          tabBarIcon: ({ color, size }) => (
+            <IconX
+              name="hand-point-up"
+              color={color}
+              size={size}
+              origin={ICON_TYPE.FONT_AWESOME5}
+              style={{}}
+            />
+          ),
         }}
         component={HomeStack}
       />
-      <Tab.Screen name="Créer" component={MyAnnouncementsStack} />
       <Tab.Screen
-        name={Routes.MY_ANNOUNCEMENTS_STACK}
+        name="Créer"
         options={{
-          title: "Mes annonces",
+          tabBarLabel: "Créer",
+          tabBarIcon: ({ color, size }) => (
+            <IconX
+              name="add-box"
+              color={color}
+              size={size + 14}
+              origin={ICON_TYPE.MATERIAL_ICONS}
+              style={{}}
+            />
+          ),
         }}
         component={MyAnnouncementsStack}
       />
-      <Tab.Screen name="Compte" component={MyAnnouncementsStack} />
+      <Tab.Screen
+        name={Routes.MY_ANNOUNCEMENTS_STACK}
+        options={{
+          tabBarLabel: "Mes annonces",
+          tabBarIcon: ({ color, size }) => (
+            <IconX
+              name="bullhorn"
+              color={color}
+              size={size}
+              origin={ICON_TYPE.FONT_AWESOME}
+              style={{}}
+            />
+          ),
+        }}
+        component={MyAnnouncementsStack}
+      />
+      <Tab.Screen
+        name="Compte"
+        options={{
+          tabBarLabel: "Compte",
+          tabBarIcon: ({ color, size }) => (
+            <IconX
+              name="user"
+              color={color}
+              size={size}
+              origin={ICON_TYPE.FONT_AWESOME}
+              style={{}}
+            />
+          ),
+        }}
+        component={MyAnnouncementsStack}
+      />
     </Tab.Navigator>
   );
 };
