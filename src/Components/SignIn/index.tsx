66133e74be02d848974a32f598248787a2efba54
Abdelhadi Habchi <abdelhadi.deve@gmail.com>
2021-01-28 15:10:28 +0100

    add signIn screen

---
+++ src/Components/SignIn/index.tsx
@@ -0,0 +1,127 @@
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import {
+  Image,
+  View,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  SafeAreaView,
+  Dimensions,
+} from "react-native";
+import { connect } from "react-redux";
+import {
+  Text,
+  Input,
+  useTheme,
+  Avatar,
+  Divider,
+  Button,
+} from "@ui-kitten/components";
+import { Colors } from "./../../Constants";
+import { IconX, ICON_TYPE } from "../../Icons";
+
+import { UploadedImageType } from "../../Store/UploadedImages/types";
+const deviceHeight = Dimensions.get("window").height;
+
+interface RootState {
+  UploadedImages: Array<UploadedImageType>;
+}
+const mapStateToProps = (state: RootState, ownProps) => ({
+  uploadedImages: state.UploadedImages,
+  ownProps: ownProps,
+});
+
+const connector = connect(mapStateToProps);
+type Props = ReturnType<typeof mapStateToProps>;
+
+const renderGmailIcon = () => (
+  <IconX
+    name="gmail"
+    size={25}
+    origin={ICON_TYPE.MATERIAL_COMMUNITY}
+    color="#fff"
+  />
+);
+
+/**
+ * SignIn
+ *
+ */
+const SignIn: React.FC<Props> = () => {
+  return (
+    <View style={styles.container}>
+      <View style={[styles.mainWrapper]}>
+        <View style={styles.introduction}>
+          <Avatar
+            size="giant"
+            source={require("../../../assets/screen-images/welcome-icon.png")}
+          />
+          <Text style={styles.title}>Bienvenue</Text>
+        </View>
+        <Text category="p2">
+          <Text style={styles.appName}>Takaful</Text>, c'est la premiere
+          solution de dons au Maroc
+        </Text>
+        <View style={styles.divider} />
+        <View style={styles.signInBlock}>
+          <Text style={styles.connectText}>Connexion</Text>
+          <Button accessoryLeft={renderGmailIcon} style={styles.installButton}>
+            SE CONNECTER AVEC GMAIL
+          </Button>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+  },
+  mainWrapper: {
+    marginHorizontal: 15,
+    marginTop: 20,
+    flex: 1,
+  },
+  inputsView: {
+    marginBottom: 10,
+  },
+  signUpBtn: {
+    width: "100%",
+    position: "absolute",
+    bottom: 130,
+  },
+  appName: { fontWeight: "bold" },
+  BottomBtns: {
+    width: "100%",
+    position: "absolute",
+    bottom: 0,
+  },
+  introduction: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 4,
+  },
+  title: {
+    marginHorizontal: 10,
+    fontSize: 20,
+  },
+  installButton: {
+    marginVertical: 4,
+    width: "100%",
+  },
+  divider: {
+    marginVertical: 5,
+    borderBottomColor: "#95a5a6",
+    borderBottomWidth: 1,
+  },
+  signInBlock: {
+    justifyContent: "center",
+    flex: 1,
+    alignItems: "center",
+  },
+  connectText: { fontSize: 30, fontWeight: "bold" },
+});
+
+export default connector(SignIn);
--- src/Screens/AccountScreen.tsx
+++ src/Screens/AccountScreen.tsx
@@ -3,17 +3,17 @@ import { View, useWindowDimensions, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import Account from "./../Components/Account";
+import SignIn from "./../Components/SignIn";
 import { ScreenProps } from "../Navigation/Routes";
 
 const AccountScreen: React.FC<ScreenProps> = ({
   route,
   navigation,
 }: ScreenProps) => {
+  const isLoggedIn = false;
   return (
     <View style={styles.container}>
-      <View>
-        <Account />
-      </View>
+      <View>{isLoggedIn ? <Account /> : <SignIn />}</View>
     </View>
   );
 };
